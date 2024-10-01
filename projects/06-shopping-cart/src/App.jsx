b0a542780fba3a686a657a4d81245ebb72a6978c
Esteban Guzman Jimenez <esguzjimenez232@gmail.com>
2024-09-30 18:08:32 -0600

    Commit de json de products

---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,12 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+
+
+function App() {
+  
+  return (
+    <Products products={products} />
+  )
+}
+
+export default App
